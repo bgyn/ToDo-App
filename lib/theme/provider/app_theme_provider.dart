25fee0027b230e403b18f8a927cc9a421aa7d260
Bigyan Thanait <bigyanthanait8@gmail.com>
2023-09-30 18:59:53 +0545

    add theme local storage

--- lib/theme/provider/app_theme_provider.dart
+++ lib/theme/provider/app_theme_provider.dart
@@ -1,12 +1,35 @@
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class AppThemeNotifer extends StateNotifier<bool> {
-  AppThemeNotifer([bool? isDarkTheme]) : super(isDarkTheme = false);
+  final ThemeLocalStorage _themeLocalStorage = ThemeLocalStorage();
+  bool? isDarkTheme;
+  AppThemeNotifer([isDarkTheme]) : super(isDarkTheme ?? false) {
+    initializeTheme();
+  }
 
-  void changeTheme() {
+  Future<void> initializeTheme() async {
+    isDarkTheme = await _themeLocalStorage.getTheme();
+    state = isDarkTheme!;
+  }
+
+  void changeTheme() async {
+    await _themeLocalStorage.setTheme(!state);
     state = !state;
   }
 }
 
 final appThemeProvider =
     StateNotifierProvider<AppThemeNotifer, bool>((_) => AppThemeNotifer());
+
+class ThemeLocalStorage {
+  Future<bool> getTheme() async {
+    final pref = await SharedPreferences.getInstance();
+    return pref.getBool('isdark') ?? false;
+  }
+
+  Future<void> setTheme(bool isDark) async {
+    final pref = await SharedPreferences.getInstance();
+    await pref.setBool('isdark', isDark);
+  }
+}
